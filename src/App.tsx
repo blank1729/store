aa8bec3a9d75fcac2fe1d87e9d22598969092daa
blank <85779539+blank1729@users.noreply.github.com>
2023-05-18 13:17:27 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div className='text-6xl underline text-blue-600'>Hello</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
